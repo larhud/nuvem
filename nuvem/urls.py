c8e40acb96c314835b4dda805263554761199eb3
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2023-07-03 18:16:55 -0300

    Upload de arquivos grandes

--- nuvem/urls.py
+++ nuvem/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import new_doc
-from .views import nuvem
-
+from .views import nuvem, transcribe, NuvemUploadView, NuvemUploadCompleteView
 
 urlpatterns = [
     path('', new_doc, name="new_doc"),
     path('nuvem/<int:id>', nuvem, name="nuvem"),
-
+    path('transcribe/', transcribe, name="transcribe"),
+    path('nuvem-upload/', NuvemUploadView.as_view(), name="nuvem_upload"),
+    path('nuvem-upload-completo/', NuvemUploadCompleteView.as_view(), name="nuvem_upload_completo"),
 ]
